45c1b5226a5b94d5f8360e832620ea64bea85aa9
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-22 09:02:33 +0100

    properly configured first view

--- CORE/GAME/views.py
+++ CORE/GAME/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializers import GameSerializer
+from .models import Game
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class GameViews(APIView):
+    def get(self,request):
+        all_games = Game.objects.all() 
+        return Response(all_games)
