d978038267fcd80157e2a8ccb2debc48af8880d7
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-22 09:16:22 +0100

    string representations added

--- CORE/GAME/models.py
+++ CORE/GAME/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class GameCategory(models.Model):
     title = models.CharField(max_length=22)
 
+    def __str__(self):
+        return self.title
+
 
 class Game(models.Model):
     name = models.CharField(max_length=400)
@@ -20,3 +23,6 @@ class Game(models.Model):
     game_play = models.TextField()
     extra_notes_title = models.CharField(max_length=100)
     extra_notes_details = models.TextField()
+
+    def __str__(self):
+        return self.name
