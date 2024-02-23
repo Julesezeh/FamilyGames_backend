cdbe75a6731678060e7c02648aa8b7aca73b2d21
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-23 08:17:29 +0100

    configured routes for categories

--- CORE/GAME/urls.py
+++ CORE/GAME/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import GameViews
+from .views import GameViews, CategoryViews
 
 
 urlpatterns = [
     path("",GameViews.as_view()),
-    path("<int:pk>", GameViews.as_view())
+    path("<int:pk>", GameViews.as_view()),
+    path("categories",CategoryViews.as_view()),
+    path("categories/<int:pk>",CategoryViews.as_view())
 ]
