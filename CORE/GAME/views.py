86b39df2f589e7ce21e9265385977aa27bcf4407
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-23 08:18:02 +0100

    Added Category views

--- CORE/GAME/views.py
+++ CORE/GAME/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .serializers import GameSerializer
-from .models import Game
+from .serializers import GameSerializer, GameCategorySerializer
+from .models import Game, GameCategory
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -17,3 +17,16 @@ class GameViews(APIView):
         all_games = Game.objects.all() 
         game_serializer = GameSerializer(all_games,many=True)
         return Response(game_serializer.data)
+
+
+class CategoryViews(APIView):
+    def get(self, request, pk=None):
+        if pk is not None:
+            selected_category = get_object_or_404(GameCategory,pk=pk)
+            if selected_category:
+                games = selected_category.game
+                serialized_games = GameSerializer(games)
+                return Response(serialized_games.data)
+        categories_qs = GameCategory.objects.all()
+        categories = GameCategorySerializer(categories_qs,many=True)
+        return Response(categories.data)
