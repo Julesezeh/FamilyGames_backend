c802edc76ebc9088230491fe5576f98b24f82fcc
Jules Okoye-ezeh <julesezeh23@gmail.com>
2024-02-23 08:15:41 +0100

    Added Game serializer

--- CORE/GAME/serializers.py
+++ CORE/GAME/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Game
+from .models import Game, GameCategory
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
+        fields = "__all__"
+
+
+class GameCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GameCategory
         fields = "__all__"
